e543b45ead4a529bcee157dae2cac2417683d10b
LinikerNogueiraMachado <linikernogueiramachado@gmail.com>
2022-08-08 15:35:21 -0300

    projeto finalizado

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,20 +1,50 @@
 let menu = document.querySelector('#menu-bars');
 let navbar = document.querySelector('.navbar');
 
-menu.onclick = () =>{
+menu.onclick = () => {
     menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active'); 
+    navbar.classList.toggle('active');
 }
 
-window.onscroll  = () =>{
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header navbar a');
+
+window.onscroll = () => {
+
     menu.classList.remove('fa-times');
-    navbar.classList.remove('active'); 
+    navbar.classList.remove('active');
+
+    section.forEach(sec => {
+
+        let top = window.scrollY;
+        let height = sec.offsetHeight;
+        let offset = sec.offsetTop -150;
+        let id = sec.getAttribute('id');
+
+        if(top => offset && top < offset + height){
+            navLinks.forEach(links =>{
+
+                links.classList.remove('active');
+                document.querySelector('header, navbar, a[href*='+id+']').classList.add('active');
+            });
+        };
+
+    });
 }
 
-document.querySelector('#search-icon').onclick = () =>{
+document.querySelector('#search-icon').onclick = () => {
     document.querySelector('#search-form').classList.toggle('active');
 }
 
-document.querySelector('#close').onclick = () =>{
+document.querySelector('#close').onclick = () => {
     document.querySelector('#search-form').classList.remove('active');
 }
+
+function loading(){
+    document.querySelector( '.loading-container').classList.add('fade-Out');
+}
+
+function fadeOut(){
+    setInterval(loading, 4000);
+}
+window.onload = fadeOut;
